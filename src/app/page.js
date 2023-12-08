1417ca37fed9d454c201cd081d415af95ffc8243
rastuak <rastuakfaisal@gmail.com>
2023-12-08 22:34:32 +0700

    second commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,93 @@
-import Image from 'next/image'
+"use client"
+
+import React from "react";
+import Typewriter from 'typewriter-effect';
+
 
 export default function Home() {
+  const mytext = [
+    'haiii gimana kabarnyaa?',
+    'baik baik kann?',
+    'harus baikbaikk lohh!!',
+    'hmmm',
+    'mulai dari manaa yaa',
+    'oyaa ini kyanya bakal lamaa dehh, jdi kmu boleh bgt buat lgsg close aja ehehe...',
+    'dan ini jga bakal ab gaaktifin lgi webnya ehehe',
+    'eh.. masi dsnii? ^-^',
+    'makasi yaa udaa ngeluangin waktunyaa',
+    'hmm ab minta maap dulu ggara ga brani langsung ngomong ke kamu',
+    'maap juga malah pake ginian buat trus terangnyaa ehehehe',
+    'jjur abi gtau harus mulai drimanaa ngmonginnyaa',
+    'oiyaa ab bkin gnian juga bkan brati abi nyari simpati kamu atw apapun yaa',
+    'inima murni wee yg abi pen omongin ke kamu',
+    'jdii gnii',
+    'sbnernya ab jgaa aneh ke diri sndiri knapa sbgai cowo ab labil bgt',
+    'ab gtauu knapa ab wktuitu kyagtu kkamuu',
+    'tiap hari ab slalu nhann diri buat ngchat kmuu dann nyari nyari ksibukan biar ga kpkiran hehehe',
+    'yaa emg ab yg bilang udh ajaa',
+    'tpi...',
+    'seinget ab, ab jga gprna bilang ab uda gada rasa',
+    'oyaa wktu itu ab smpet ngliat sg sec acc kamu klo gasalah dan ab pen reply pas itu tpi abi tahan trus smpe lupa ehehe',
+    'tebakk ab mau reply gmnaaa',
+    'salahhh',
+    'ab mw reply klo kmu emg slalu ab anggap rumahh ehehe',
+    'ehehe',
+    'mungkin emg bahasa "buaya" yakk tpi balik lgi abi bkin gnian cuma buat nyampein apa yg ab pen omongin ke kamu..',
+    'trus abis itu tbatba ab gpernah lagii tuu ngliat sg kamu',
+    'ab kiraa emang kmu yg ga bikin sg ajaa',
+    'ab bru ngeh kmu removee ab pas ada notif theme dmnyaa dganti..',
+    'jd yaa ab gbsa nahan diri abi buat ngchat kmuu wktuitu dann akhirnya ab bkin gnian jugaa ehehee..',
+    'ab sbnernya gamau ngganggu kamu lagii',
+    'cmaa yaa... maapin yaakkkk',
+    'ab emang jelek komunikasinya, dalam sgala hal',
+    'dpikiran ab banyak yg mau diomongin tpi gtau knapa yg bneran dsampeinnya cuma bberapa',
+    'mkanyaa kkamugee ab sering bgt dikit balesannyaa dri chat kmu yg bnyak',
+    'tpi ab jga gmau ngeles,',
+    'ab bnyak banget nyadar klo ab bkann yg trbaik bwt kamuu',
+    'mkanya ab jga ksel k diri sndirii',
+    'dtambah wktuitu bnyak pkiran dri tugas, tuntutan, smaa',
+    'keluarga ehehe',
+    'bsa dbilang ab wktuitu udh nyerahh bgt sama smuanya',
+    'cape bgtt',
+    'tpi ab gngerti bgt knapa ab jd kyagtu kkamu..',
+    'maapp bgtt',
+    'truss apaa yaaa.. bingung jugaa ehehe..',
+    'oyaaa',
+    'mungkin emang blm waktunya lagi kita smasmaa',
+    'atw mungkin emg udh gkan smasmaa lagii',
+    'tpi ttep....',
+    'ab slalu anggap kmu rumah ehehe',
+    'tpi ab jgaa gppa klo misal kmu emg udh ada org lain..',
+    'biarin abii tauu klo emg kyagitu yakk biar ab juga tenang ajaa gtuu',
+    'eh gamaksa sii tserah kmu jugaa ehehe',
+    'oyaa',
+    'gtau knpaa tpi minggu ini kyanya ab sring banget mmpiin kmuu dan ab slalu mnta maap k kamuu tpi kya gprna dtanggepin gtu :(',
+    'oyaa trus ab jga emg udh ganti pp wa, bio ig, tpi ab jga gtau knpa dri wktuitu ab gbisa buat ngganti bio wa abi..',
+    'kya emg diri ab yg nglarang aja gitu',
+    'hmm trus apalagi yaa,',
+    'bingung si..',
+    'udah deh segini aja ehehehe',
+    'skali lagi mkasi uda ngeluangin wktunyaa buat bacain teks panjang gini yg gapenting sma gjelas gnii wkwkwk',
+    'baiii raee',
+  ]
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main>
+      <div className="flex justify-center items-center h-screen">
+        <div className="flex justify-center items-center min-h-screen">
+          <div className='w-auto p-7 rounded-lg text-justify text-xl font-bold'>
+            <Typewriter
+              options={{
+                strings: mytext,
+                autoStart: true,
+                loop: false,
+                deleteSpeed: 5,
+                delay: 65,
+              }}
             />
-          </a>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
